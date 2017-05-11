24bd85afecce0cb8036aa15f661fe51ebe40471a
SourabhKushwaha <kussourabh2008@gmail.com>
2017-05-11 12:36:10 +0530

    Adds SerialRW
    Demonstrate how to read/write serial data on port. 
    Imp bound rate is very important (should be match in both app)

---
+++ Arduino/SerialRW.ino
@@ -0,0 +1,37 @@
+int incomingByte = 0;   // for incoming serial data
+int outLEDPin = LED_BUILTIN;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Started Listening");  
+  pinMode(outLEDPin, OUTPUT); // initialize digital pin LED_BUILTIN as an output.
+}
+void serialReadEvent()
+{
+   // send data only when you receive data:
+        if (Serial.available() > 0) {
+                incomingByte = Serial.read(); // read the incoming byte:
+             
+                Serial.print(incomingByte, DEC);
+                Serial.print(",");
+
+                if(incomingByte == 115){
+                  glowLED(1000);
+                }
+        }
+}
+
+void glowLED(int t){
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(t);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  serialReadEvent();
+  /*Serial.println("1234");
+  glowLED(1000);
+  delay(1000);                       // wait for a second*/
+}
