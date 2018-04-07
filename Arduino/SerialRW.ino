ced7eda76f214cb1a10e4cd6e1741cdcc2554cfa
SourabhKushwaha <kussourabh2008@gmail.com>
2018-04-07 16:36:59 +0530

    Update SerialRW.ino

--- Arduino/SerialRW.ino
+++ Arduino/SerialRW.ino
@@ -1,14 +1,18 @@
-int incomingByte = 0;   // for incoming serial data
-int outLEDPin = LED_BUILTIN;
+short int incomingByte  = 0;   // for incoming serial data
+short int outLEDPin     = LED_BUILTIN;
+short int inPin         = 7;     // pushbutton connected to digital pin 7
+int val                 = 0;       // variable to store the read value
+short int counter       = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Started Listening");  
   pinMode(outLEDPin, OUTPUT); // initialize digital pin LED_BUILTIN as an output.
+  pinMode(inPin, INPUT);        // sets the digital pin 7 as input
 }
-void serialReadEvent()
-{
+
+void serialReadEvent() {
    // send data only when you receive data:
         if (Serial.available() > 0) {
                 incomingByte = Serial.read(); // read the incoming byte:
@@ -16,13 +20,13 @@ void serialReadEvent()
                 Serial.print(incomingByte, DEC);
                 Serial.print(",");
 
-                if(incomingByte == 115){
+                if(incomingByte == 115) {
                   glowLED(1000);
                 }
         }
 }
 
-void glowLED(int t){
+void glowLED(int t) {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(t);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
@@ -30,8 +34,15 @@ void glowLED(int t){
 
 // the loop function runs over and over again forever
 void loop() {
+  counter++;
+  unsigned long currentMillis = millis();
   serialReadEvent();
-  /*Serial.println("1234");
-  glowLED(1000);
-  delay(1000);                       // wait for a second*/
+
+  if(counter == 1000) {
+    counter = 0;
+    val = digitalRead(inPin);     // read the input pin
+    Serial.println(val);
+  }  
+
+  delay(1);
 }
